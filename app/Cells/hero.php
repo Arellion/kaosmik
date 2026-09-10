5c07149072dd7f04f00ae52013e9ec78f782c6e8
Arellion <leo.pommarede@gmail.com>
2026-09-10 17:00:21 +0200

    fix : cantina (#11)

--- app/Cells/hero.php
+++ app/Cells/hero.php
@@ -2,4 +2,10 @@
     <div class="card-header">
         <?= $character->name ?>
     </div>
+    <div class="card-body">
+        <ul>
+            <li>power : <?= $character->power ?></li>
+            <li>cost : <?= $character->cost_credit ?></li>
+        </ul>
+    </div>
 </div>
