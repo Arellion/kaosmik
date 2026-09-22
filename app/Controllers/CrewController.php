960d1472596e48da1aac1d8b1f4f243232e5d28e
Arellion <leo.pommarede@gmail.com>
2026-09-22 16:02:26 +0200

    feat : crew selection multiple

--- app/Controllers/CrewController.php
+++ app/Controllers/CrewController.php
@@ -8,6 +8,12 @@ use CodeIgniter\HTTP\ResponseInterface;
 class CrewController extends BaseController
 {
     protected $current_menu = 'crew';
+    protected $heroModel = null;
+
+    public function __construct()
+    {
+        $this->heroModel = model('HeroModel');
+    }
     public function index()
     {
         helper(['form']);
@@ -17,18 +23,42 @@ class CrewController extends BaseController
     public function sell($id_heroes = null)
     {
         if (!empty($id_heroes)) {
-            $heroModel = model('HeroModel');
-            $hero = $heroModel->find($id_heroes);
+            $hero = $this->heroModel->find($id_heroes);
             $player = auth()->user()->getplayer();
             if($hero->player_id == $player->id) {
                 $argent = (int) ($hero->cost_credit / 2);
-                if($heroModel->delete($id_heroes)) {
+                if($this->heroModel->delete($id_heroes)) {
                     $player->credits += $argent;
                     if(model('PlayerModel')->save($player)){
                         $this->success($hero->name. "à été licencié. Vous récupérez <i class='fa-solid fa-cent-sign'></i>". $argent . "." );
                         return $this->redirect('equipage');
                     }
                 }
+            }
+        }
+        $this->error('Une erreur est survenue. Veuillez contacter un administrateur');
+        return $this->redirect('/equipage');
+    }
+
+    public function sellBulk()
+    {
+        $ids = $this->request->getPost('ids');
+        if(!empty($ids) && is_array($ids)) {
+            $player = auth()->user()->getplayer();
+            $heroes = $this->heroModel->whereIn('id', $ids)->where('player_id', $player->id)->findAll();
+
+            $totalGain = 0;
+            $deletedIds = array();
+            foreach ($heroes as $hero) {
+                $totalGain += (int) ($hero->cost_credit / 2);
+                $deletedIds[] = $hero->id;
+            }
+            if(!empty($deletedIds)) {
+                $this->heroModel->delete($deletedIds);
+                $player->credits += $totalGain;
+                model('PlayerModel')->save($player);
+                $this->success(count($deletedIds) . "mercenaires ont été licenciés. Vous avez récupéré <i class='fa-solid fa-cent-sign'></i>" . $totalGain . "." );
+                return $this->redirect('equipage');
             }
         }
         $this->error('Une erreur est survenue. Veuillez contacter un administrateur');
--- app/Views/front/crew/index.php
+++ app/Views/front/crew/index.php
@@ -1,25 +1,38 @@
 <div class="row">
     <div class="col d-flex">
         <div>
-            <h1 class="shadow text-white"> Mon équipages' </h1>
+            <h1 class="shadow text-white"> Mon équipages </h1>
             <span class="text-white">Ici, on gère nos mercenaires</span>
         </div>
     </div>
-</div>
 
-<div class="row row-cols-6">
-    <?php
-    foreach ($logged_user->getplayer()->getHeroes() as $hero) { ?>
-        <div class="col mb-3">
-            <?= view_cell('HeroCell', ['character' => $hero, 'context' => 'crew']) ?>
-        </div>
-    <?php } ?>
+    <div class="col-auto d-flex g-3">
+        <button type="button" class="btn btn-outline-danger" id="toggle-bulk-mode">
+            Licensier en masse
+        </button>
+
+        <?= form_open('equipage/sell-bulk', ['id' => 'form-sell-bulk', 'class' => 'd-none']) ?>
+        <div id="bulk-inputs"></div>
+        <button type="submit" id="btn-sell-bulk" class="btn btn-danger" disabled>
+            Licencier la selection ( <i class="fa-solid fa-cent-sign"></i> <span id="bulk-price">0</span>)
+        </button>
+        <?= form_close() ?>
+    </div>
+    <div class="row row-cols-6 g-3">
+        <?php
+        foreach ($logged_user->getplayer()->getHeroes() as $hero) { ?>
+            <div class="col mb-3">
+                <?= view_cell('HeroCell', ['character' => $hero, 'context' => 'crew']) ?>
+            </div>
+        <?php } ?>
+    </div>
 </div>
 
 <script>
     document.addEventListener('DOMContentLoaded', () => {
-        document.querySelectorAll('.js-form-sell').forEach(form => {
-            form.addEventListener('submit', function(e) {
+        //Gestion de la confirmation pour la vente individuelle
+        document.querySelectorAll('.js-single-form-sell').forEach(form => {
+            form.addEventListener('submit', function (e) {
                 e.preventDefault();
                 const btn = this.querySelector('button[type="submit"]');
                 const heroName = btn.dataset.heroName || 'ce mercenaire';
@@ -34,11 +47,128 @@
                         confirmButton: 'btn btn-kaosmik'
                     }
                 }).then((result) => {
-                    if(result.isConfirmed){
+                    if (result.isConfirmed) {
                         this.submit();
                     }
                 })
+            });
+        });
+
+        //Déclaration des variables pour la vente en lot
+        const toggleBtn = document.getElementById('toggle-bulk-mode');
+        const bulkForm = document.getElementById('form-sell-bulk');
+        const bulkInputs = document.getElementById('bulk-inputs');
+        const bulkPrice = document.getElementById('bulk-price')
+        const btnSellBulk = document.getElementById('btn-sell-bulk')
+
+        //Bouton de bascule pour la vente en lot
+        toggleBtn.addEventListener('click', () => {
+            const isBulkInactive = bulkForm.classList.contains('d-none');
+
+            if (isBulkInactive) {
+                //Activation de la vente en lot
+                bulkForm.classList.remove('d-none')
+                toggleBtn.classList.replace('btn-outline-danger', 'btn-warning')
+                toggleBtn.textContent = 'Annuler';
+
+            } else {
+                //Désactivation de la vente en lot
+                bulkForm.classList.add('d-none')
+                toggleBtn.classList.replace('btn-warning', 'btn-outline-danger')
+                toggleBtn.textContent = 'Licensier en masse'
+
+                //Décocher toute les case
+                document.querySelectorAll('.js-hero-select').forEach(el => {
+                    el.checked = false
+                });
+                updateBulkTotal();
+            }
+
+            document.querySelectorAll('.js-bulk-checkbox-container').forEach(el => {
+                el.classList.toggle('d-none', !isBulkInactive)
+            });
+            document.querySelectorAll('.js-single-sell-form').forEach(el => { //todo
+                el.classList.toggle('d-none', isBulkInactive)
             })
         })
-    })
+
+        //Selection du clic sur la carte pour cacher / décocher
+        document.querySelectorAll('.js-hero-card').forEach(card => {
+            card.addEventListener('click', (e) => {
+                //Si la vente en lot est ACTIVE et que l'on ne click pas DIRECTEMENT dans la checkbox
+                if (!bulkForm.classList.contains('d-none') && !e.target.classList.contains('js-hero-select')) {
+                    const checkbox = card.querySelector('.js-hero-select');
+                    if (checkbox) {
+                        checkbox.click();
+                    }
+                }
+            })
+        })
+
+        document.querySelectorAll('.js-hero-select').forEach(checkbox => {
+            checkbox.addEventListener('change', updateBulkTotal)
+        })
+
+        //Confirmation Swal2 pour la vente en lot
+        bulkForm.addEventListener('submit', function (e) {
+            e.preventDefault();
+            const total = parseInt(bulkPrice.textContent) || 0;
+
+            Swal.fire({
+                title: 'Voulez vous vraiment licencier ces mercenaires ?',
+                text: `Vous allez licencier pour ${total} crédits`,
+                showCancelButton: true,
+                confirmButtonText: 'Oui !',
+                cancelButtonText: 'Annuler',
+                icon: 'warning',
+                customClass: {
+                    confirmButton: 'btn btn-kaosmik'
+                }
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    this.submit();
+                }
+            })
+        })
+
+        //Recalcul du total
+        function updateBulkTotal() {
+            let total = 0;
+            let count = 0;
+
+            bulkInputs.innerHTML = '';
+            document.querySelectorAll('.js-hero-select').forEach(checkbox => {
+                const card = checkbox.closest('.js-hero-card');
+
+                if (checkbox.checked) {
+
+                    card.classList.add('is-selected');
+                    const price = parseInt(card.dataset.sellPrice) || 0;
+                    const heroId = card.dataset.id
+                    total += price;
+                    count++
+
+
+                    //Ajouter mon input id caché
+                    const hiddenInput = document.createElement('input')
+                    hiddenInput.type = 'hidden';
+                    hiddenInput.name = 'ids[]';
+                    hiddenInput.value = heroId;
+                    bulkInputs.appendChild(hiddenInput);
+                }else{
+                    card.classList.remove('is-selected')
+                }
+            });
+
+            bulkPrice.textContent = total;
+            btnSellBulk.disabled = (count === 0 || count === 1);
+        }
+    });
 </script>
+
+<style>
+    .js-hero-card.is-selected {
+        filter: brightness(0.6);
+        opacity: 0.8;
+    }
+</style>
